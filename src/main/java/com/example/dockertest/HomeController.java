885b27e1244c36c49f800fa447359e15dc685d21
jdi022222 <jdi022222@gmail.com>
2023-12-11 16:29:37 +0900

    deploy test
    - add cd.yml
    - delete profile
    - delete docker-compose.yml

--- src/main/java/com/example/dockertest/HomeController.java
+++ src/main/java/com/example/dockertest/HomeController.java
@@ -17,35 +17,4 @@ public class HomeController {
 		return "Home";
 	}
 
-	@RestController
-	public class ServerController {
-		@Value("${serverName}") // application.yml의 값들을 매핑시킴
-		private String serverName;
-
-		@Value("${dbInfo}")
-		private String dbInfo;
-
-		@Value("${server.env}")
-		private String env;
-
-		private Integer visitedCount = 0;
-
-		@GetMapping("/getServerInfo")
-		public ResponseEntity<Map<String, String>> getServerInfo() {
-			visitedCount++;
-
-			Map<String, String> serverInfo = new HashMap<>();
-			serverInfo.put("ServerName:", serverName);
-			serverInfo.put("visitedCount:", visitedCount.toString());
-			serverInfo.put("dbInfo", dbInfo);
-			serverInfo.put("env:", env);
-
-			return ResponseEntity.ok(serverInfo);
-		}
-
-		@GetMapping("/env")
-		public String getEnv() {
-			return env;
-		}
-	}
 }
